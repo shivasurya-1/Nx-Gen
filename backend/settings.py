d412428a3a4f43d83c930b4d52d3ad1d5b882844
shivasurya-1 <shivasurya821@gmail.com>
2026-04-03 12:04:41 +0530

    Patch cloudinary compatibility bug

--- backend/settings.py
+++ backend/settings.py
@@ -157,6 +157,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
